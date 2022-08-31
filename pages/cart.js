d997314adba034696f40c9918cda72a6471ecc3a
Pablo Lepratti <pablolepratti@gmail.com>
2022-08-31 08:39:57 -0300

    Update 2

---
+++ pages/cart.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+
+
+export default function Cart() {
+  return (
+    <Layout title="Carrito" >
+      Home Page
+    </Layout>
+  )
+}
---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+
+
+export default function Login() {
+  return (
+    <Layout title="Ingresar" >
+      Home Page
+    </Layout>
+  )
+}
